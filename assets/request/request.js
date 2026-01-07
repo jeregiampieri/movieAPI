3fe45901e5a07cda3966de8cce3a739b6b91a234
Jeremias <jeregiampieri@gmail.com>
2026-01-07 14:10:37 -0300

    actualizaciones

--- assets/request/request.js
+++ assets/request/request.js
@@ -1,7 +1,9 @@
+export const key = "3c4abaf7eeeeaea38ab0d382b5aff605"
+
 // Función para poder establecer la conexión con la API
-export const requestAPI = async() => {
+export const requestAPI = async(url) => {
     try{
-        const response = await fetch(`https://openweathermap.org/api`)
+        const response = await fetch(url)
         const data = await response.json()
         console.log(data)
         return data
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-import { requestAPI } from "./assets/request/request.js";
+import { requestAPI, key } from "./assets/request/request.js";
+
+// Constantes para mantener las URL necesarias para insertarles la API key
+const urlTrending = `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}&language=en-US`
+const urlTopRates = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`
+const urlUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
+const urlImg = `https://image.tmdb.org/t/p/original`
 
 // Función inicializadora de la aplicación
 const init = () => {
-    requestAPI()
+    requestAPI(urlUpComing)
 }
 
 init()
+
