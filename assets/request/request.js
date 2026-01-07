e8f4cdbdb1d7a955ef29a3d682af1ab39ddaaf0f
Jeremias <jeregiampieri@gmail.com>
2026-01-07 15:08:20 -0300

    actualizaciones

--- assets/request/request.js
+++ assets/request/request.js
@@ -1,11 +1,10 @@
 export const key = "3c4abaf7eeeeaea38ab0d382b5aff605"
 
 // Función para poder establecer la conexión con la API
-export const requestAPI = async(url) => {
+export const requestAPI = async(url, page = 1) => {
     try{
-        const response = await fetch(url)
+        const response = await fetch(url + `&page=${page}`)
         const data = await response.json()
-        console.log(data)
         return data
     }catch(error){
         console.log(error)
--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import { requestAPI, key } from "./assets/request/request.js";
 
 // Constantes para mantener las URL necesarias para insertarles la API key
+// A estos endpoints los estoy pasando por query params (esto lo puedo ver en la documentación de la API), en este caso, los params que estoy usando
+// sería language y page
 const urlTrending = `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}&language=en-US`
-const urlTopRates = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`
+const urlTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`
 const urlUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
 const urlImg = `https://image.tmdb.org/t/p/original`
 
+// Objeto para poder mantener el estado actual de la aplicación
+const appState = {
+    page: null,
+    // Creo este atributo para poder hacer el control y validación de que el usuario no exceda el total máximo de páginas
+    totalPage: null,
+    url: urlTrending
+}
+
 // Función inicializadora de la aplicación
 const init = () => {
-    requestAPI(urlUpComing)
+    requestAPI(urlTrending,5)
 }
 
 init()
